5d7fe14b283a5c62455235c0d3693497c31279de
lrzeszotarski <lukas.rzeszotarski@outlook.com>
2020-09-17 13:57:56 +0200

    Bridge Scala

---
+++ Scala/structural/Bridge.scala
@@ -0,0 +1,42 @@
+package structural
+
+import scala.util.Random
+
+object Bridge extends App {
+
+  trait RightBank {
+    def explode: Any = println("Triggering explosion mechanism of the Bridge")
+  }
+
+  abstract class LeftBank(var rightBank: RightBank) {
+    def reactToMilitaryConflict: Any = rightBank.explode
+  }
+
+  class RightBankWhenWar extends RightBank {
+    override def explode: Any = println("Triggering explosion of the Bridge")
+  }
+
+  class RightBankWhenPeace extends RightBank {
+    override def explode: Any = println("There is no real military conflict. No explosion during security training.")
+  }
+
+  class LeftBankInWarAndPeace(rightBank: RightBank) extends LeftBank(rightBank) {
+    override def reactToMilitaryConflict: Any = {
+      println("take own military resources to the left bank")
+      super.reactToMilitaryConflict
+    }
+  }
+
+  val rightBankWhenWar = new RightBankWhenWar
+  val rightBankWhenPeace = new RightBankWhenPeace
+  val leftBank = new LeftBankInWarAndPeace(rightBankWhenPeace)
+
+  val isThereWar = () => Random.nextBoolean()
+
+  if (isThereWar()) {
+    leftBank.rightBank = rightBankWhenWar
+  }
+
+  leftBank.reactToMilitaryConflict
+
+}
